08adb64774d1bf127d35190dd51e7eaecd76af40
YashMishra0101 <yashrkmfedu6927@gmail.com>
2024-03-23 13:26:58 +0530

    Added signup pages and fixed background image loading time on login and signup pages

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,18 +1,30 @@
-import React from "react";
-import bgimage from '../assets/loginbg.png'
+import React, { useState, useEffect } from "react";
+import bgimage from "../assets/loginbg.png";
 
 export const Login = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      <section className="relative overflow-hidden">
-        <div className="absolute inset-0 bg-gray-50">
-          <img src={bgimage} alt="Background" className="object-cover w-full h-full" />
-        </div>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 -mt-5">
+    <div className="overflow-hidden h-screen">
+      <section className="relative">
+        <div
+          className="absolute inset-0 bg-gray-50"
+          style={{
+            backgroundImage: `url(${bgimage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 md:-mt-5 pt-[7rem] md:-pt-[7rem]">
           <div className="w-full bg-white bg-opacity-50 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 relative z-10">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Sign in to your account
+                Login in to your account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
@@ -27,7 +39,7 @@ export const Login = () => {
                     name="email"
                     id="email"
                     className="bg-gray-50 bg-opacity-70 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="Enter your email id"
                     required=""
                   />
                 </div>
@@ -38,29 +50,31 @@ export const Login = () => {
                   >
                     Password
                   </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 bg-opacity-70 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 bg-opacity-70 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
                       <input
-                        id="remember"
-                        aria-describedby="remember"
+                        id="showpassword"
+                        aria-describedby="showpassword"
                         type="checkbox"
                         className="w-4 h-4 border border-gray-300 rounded bg-gray-50 bg-opacity-70 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
+                        onClick={handleShowPassword}
                       />
                     </div>
                     <div className="ml-3 text-sm">
-                      <label htmlFor="remember" className="text-gray-600">
-                        Remember me
+                      <label htmlFor="showpassword" className="text-gray-600">
+                        Show password
                       </label>
                     </div>
                   </div>
@@ -75,7 +89,7 @@ export const Login = () => {
                   type="submit"
                   className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Sign in
+                  Login
                 </button>
                 <p className="text-sm  text-gray-600">
                   Don’t have an account yet?{" "}
@@ -83,7 +97,7 @@ export const Login = () => {
                     href="#"
                     className="font-medium text-primary-600 hover:underline"
                   >
-                    Sign up
+                    Login
                   </a>
                 </p>
               </form>
