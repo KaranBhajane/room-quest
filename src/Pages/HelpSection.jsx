31e54285141ff293bbe8b8cf0daf0ccbb2146b89
Dhanashree312 <dhanashree@gmail.com>
2024-04-06 11:56:10 +0530

    Colour issue sorted of helpsection

--- src/Pages/HelpSection.jsx
+++ src/Pages/HelpSection.jsx
@@ -24,13 +24,13 @@ export const HelpSection = () => {
             <h4 className="text-lg tracking-widest font-bold">General Questions</h4>
           </div>
           <div className="space-y-8 mt-4 lg:mt-6">
-            <Accordion>
-              <Accordion.Panel>
-                <Accordion.Title className="pl-2">
-                  What is Room Quest?
+            <Accordion className="bg-white">
+              <Accordion.Panel className="bg-white">
+                <Accordion.Title className="pl-2 ">
+                 <span className="text-gray-400">What is Room Quest?</span> 
                 </Accordion.Title>
-                <Accordion.Content>
-                  <p className="text-lg text-blue-700  p-4">
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
+                  <p className="text-lg text-blue-700 bg-white p-4">
                     Our platform serves as a marketplace where homeowners can
                     list their rooms for rent, and users can browse and apply
                     for those rooms.
@@ -42,9 +42,9 @@ export const HelpSection = () => {
             <Accordion collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="pl-2">
-                  How can I list my room for rent on this platform?
+                <span className="text-gray-400"> How can I list my room for rent on this platform?</span>
                 </Accordion.Title>
-                <Accordion.Content>
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
                   <p className="text-lg text-blue-700  p-4">
                     To list your room, you need to sign up as a homeowner,
                     provide details about your property, upload photos, and set
@@ -58,9 +58,9 @@ export const HelpSection = () => {
             <Accordion collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="pl-2">
-                  Is there a fee for listing a room on the platform?
+                <span className="text-gray-400"> Is there a fee for listing a room on the platform?</span>
                 </Accordion.Title>
-                <Accordion.Content>
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
                   <p className="text-lg text-blue-700  p-4">
                     Yes, there might be a nominal fee for homeowners to list
                     their rooms. This fee helps us maintain and improve the
@@ -73,9 +73,9 @@ export const HelpSection = () => {
             <Accordion collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="pl-2">
-                  How do I apply for a room?
+                <span className="text-gray-400">How do I apply for a room?</span>
                 </Accordion.Title>
-                <Accordion.Content>
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
                   <p className="text-lg text-blue-700  p-4">
                     To apply for a room, you need to create a user account,
                     browse listings, and submit an application for the desired
@@ -89,9 +89,9 @@ export const HelpSection = () => {
             <Accordion collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="pl-2">
-                  What information should I include in my room application?
+                <span className="text-gray-400"> What information should I include in my room application?</span>
                 </Accordion.Title>
-                <Accordion.Content>
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
                   <p className="text-lg text-blue-700  p-4">
                     Your application should include basic personal information,
                     such as your name, contact details, occupation, and a brief
@@ -106,9 +106,9 @@ export const HelpSection = () => {
             <Accordion collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="pl-2">
-                  Are there any safety measures in place for users?
+                <span className="text-gray-400">  Are there any safety measures in place for users?</span>
                 </Accordion.Title>
-                <Accordion.Content>
+                <Accordion.Content style={{ backgroundColor: 'white' }}>
                   <p className="text-lg text-blue-700 p-4">
                     We prioritize user safety and security. We verify
                     homeowners' identities and encourage users to review and
